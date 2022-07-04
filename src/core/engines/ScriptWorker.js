565dd207cb5999c0ebf6986bd0dfbf0a8f882309
Equilibrier <cosmin.popescu.13@gmail.com>
2022-07-04 18:18:15 +0300

    debug...

--- src/core/engines/ScriptWorker.js
+++ src/core/engines/ScriptWorker.js
@@ -8,7 +8,7 @@ self.addEventListener('message', e => {
 
     const js = e.data.code
     //Logger.log(3, `global-script urls: ${Object.values(this.canvas.getSettings().globalScriptUrlsEnabled)}`, e)
-    Logger.log(3, `e: ${Object.keys(e)}`, e)
+    console.log(`e: ${Object.keys(e)}`);
     const model = e.data.model
     const viewModel = e.data.viewModel
     const qux = new ScriptAPI(model)
