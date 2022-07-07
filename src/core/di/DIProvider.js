d1990368c7abe54c5fa3925494a3334354897dc9
Equilibrier <cosmin.popescu.13@gmail.com>
2022-07-07 21:21:57 +0300

    ...

---
+++ src/core/di/DIProvider.js
@@ -0,0 +1,19 @@
+
+class DIProvider {
+    constructor() {
+        this.canvas = null;
+    }
+
+    setCanvas(canvas) {
+        if (this.canvas === null) {
+            this.canvas = canvas;
+        }
+        else {
+            console.warn("Canvas already set...");
+        }
+    }
+
+    canvas() { return this.canvas }
+}
+
+export default new DIProvider();
