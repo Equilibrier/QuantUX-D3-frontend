d15dc3b27d5a8020f9af73481cd70b7199516f4d
Equilibrier <cosmin.popescu.13@gmail.com>
2022-07-07 21:54:55 +0300

    debug..

--- src/core/engines/ScriptEngine.js
+++ src/core/engines/ScriptEngine.js
@@ -18,6 +18,7 @@ export default class ScriptEngine {
                     code: js, 
                     model: lang.clone(model), 
                     viewModel: lang.clone(viewModel),
+                    console
                 })
 
 
